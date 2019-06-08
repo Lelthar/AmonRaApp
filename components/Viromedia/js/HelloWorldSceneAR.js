435cef04bc48762fbb95c4df264d4a44cd2368a0
josuarez2297@gmail.com <josuarez2297@gmail.com>
2019-06-07 20:25:21 -0600

    AR avance

--- components/Viromedia/js/HelloWorldSceneAR.js
+++ components/Viromedia/js/HelloWorldSceneAR.js
@@ -37,22 +37,12 @@ export default class HelloWorldSceneAR extends Component {
     this._onInitialized = this._onInitialized.bind(this);
     this._latLongToMerc = this._latLongToMerc.bind(this);
     this._transformPointToAR = this._transformPointToAR.bind(this);
-
+    this._setCompass = this._setCompass.bind(this);
     this.state = {
-      text : "Initializing AR...",
       latitude: 0,
       longitude: 0,
-      // ================================Prueba con variables estáticas =================================================
-      objectXPosFrenteH: 0,
-      objectZPosFrenteH: 0,
-      objectXPosFrenteMate: 0,
-      objectZPosFrenteMate: 0,
-      objectXPosPretil: 0,
-      objectZPosPretil: 0,
-      scaleObj1: [5,5,5],
-      scaleObj2: [5,5,5],
-      scaleObj3: [5,5,5],
-      // ================================================================================================================
+      objectXPos: 0,
+      objectZPos: 0,
       heading: 0,
       coordinateString: "Sin datos",
       coordinateLatLongString: "Sin datos",
@@ -60,69 +50,27 @@ export default class HelloWorldSceneAR extends Component {
     };
   }
 
-  componentDidMount(){
-    let myself = this;
-    const degree_update_rate = 3; // Number of degrees changed before the callback is triggered
-      RNSimpleCompass.start(degree_update_rate, (degree) => {
-        console.log('You are facing', degree);
-        myself.setState({
-          heading: degree
-        });
-        RNSimpleCompass.stop();
-      });
-  }
-
   render() { 
     return (
         <ViroARScene onTrackingUpdated={this._onInitialized}>
           {/*<ViroText text={this.state.heading+"||"+this.state.coordinateLatLongString + " || " +this.state.coordinateXYZString}
               scale={[.1, .1, .1]} height={5} width={4} position={[0, 0, -.1]} style={styles.helloWorldTextStyle} />
-          <ViroText text={"PUNTO"}
-              scale={[7, 7, 7]} height={7} width={5} position={[this.state.objectXPosFrenteH, 0, this.state.objectZPosFrenteH]} style={styles.helloWorldTextStyle} />
-
-          <ViroText text={"PUNTO"}
-              scale={[7, 7, 7]} height={7} width={5} position={[this.state.objectXPosFrenteMate, 0, this.state.objectZPosFrenteMate]} style={styles.helloWorldTextStyle} />
-
-          <ViroText text={"PUNTO"}
-              scale={[7, 7, 7]} height={7} width={5} position={[this.state.objectXPosPretil, 0, this.state.objectZPosPretil]} style={styles.helloWorldTextStyle} />
         */}
+          <ViroText text={"Casa Verde"}
+            scale={[7, 7, 7]} height={7} width={5} position={[this.state.objectXPos, 0, this.state.objectZPos]} style={styles.helloWorldTextStyle} />
 
           <ViroAmbientLight color={"#aaaaaa"} />
           <ViroSpotLight innerAngle={5} outerAngle={90} direction={[0,-1,-.2]}
             position={[0, 3, 1]} color="#ffffff" castsShadow={true} />
-          <ViroNode position={[this.state.objectXPosFrenteH, 0, this.state.objectZPosFrenteH]} dragType="FixedToWorld" onDrag={()=>{}} >
+          <ViroNode position={[this.state.objectXPos, 0, this.state.objectZPos]} dragType="FixedToWorld" onDrag={()=>{}} >
             <Viro3DObject
               onClick={this.props.arSceneNavigator.viroAppProps.showInformation}
               source={require('./res/emoji_smile/emoji_smile.vrx')}
               resources={[require('./res/emoji_smile/emoji_smile_diffuse.png'),
                   require('./res/emoji_smile/emoji_smile_normal.png'),
                   require('./res/emoji_smile/emoji_smile_specular.png')]}
-              position={[this.state.objectXPosFrenteH, 2, this.state.objectZPosFrenteH]}
+              position={[this.state.objectXPos, 2, this.state.objectZPos]}
               scale={this.state.scaleObj1}
-              type="VRX" />
-          </ViroNode>
-
-          <ViroNode position={[this.state.objectXPosFrenteMate, 0, this.state.objectZPosFrenteMate]} dragType="FixedToWorld" onDrag={()=>{}} >
-            <Viro3DObject
-              onClick={this.props.arSceneNavigator.viroAppProps.showInformation}
-              source={require('./res/emoji_smile/emoji_smile.vrx')}
-              resources={[require('./res/emoji_smile/emoji_smile_diffuse.png'),
-                  require('./res/emoji_smile/emoji_smile_normal.png'),
-                  require('./res/emoji_smile/emoji_smile_specular.png')]}
-              position={[this.state.objectXPosFrenteMate, 2, this.state.objectZPosFrenteMate]}
-              scale={this.state.scaleObj2}
-              type="VRX" />
-          </ViroNode>
-
-          <ViroNode position={[this.state.objectXPosPretil, 0, this.state.objectZPosPretil]} dragType="FixedToWorld" onDrag={()=>{}} >
-            <Viro3DObject
-              onClick={this.props.arSceneNavigator.viroAppProps.showInformation}
-              source={require('./res/emoji_smile/emoji_smile.vrx')}
-              resources={[require('./res/emoji_smile/emoji_smile_diffuse.png'),
-                  require('./res/emoji_smile/emoji_smile_normal.png'),
-                  require('./res/emoji_smile/emoji_smile_specular.png')]}
-              position={[this.state.objectXPosPretil, 2, this.state.objectZPosPretil]}
-              scale={this.state.scaleObj3}
               type="VRX" />
           </ViroNode>
 
@@ -140,21 +88,31 @@ export default class HelloWorldSceneAR extends Component {
         </ViroARScene>
     );
   }
+
+  componentDidMount(){
+    this._setCompass();
+  }
   
+  _setCompass(){
+    let myself = this;
+    const degree_update_rate = 3; // Number of degrees changed before the callback is triggered
+      RNSimpleCompass.start(degree_update_rate, (degree) => {
+        console.log('You are facing', degree);
+        myself.setState({
+          heading: degree
+        });
+        RNSimpleCompass.stop();
+      });
+  }
   _transformPointToAR(lat, long,latPlace,LongPlace) {
-    
-    var objPoint = this._latLongToMerc(latPlace, LongPlace); 
-    var devicePoint = this._latLongToMerc(lat, long);
+    let objPoint = this._latLongToMerc(latPlace, LongPlace); 
+    let devicePoint = this._latLongToMerc(lat, long);
     
     // latitude(north,south) maps to the z axis in AR
     // longitude(east, west) maps to the x axis in AR
 
-    var objFinalPosZ = objPoint.y - devicePoint.y;
-    var objFinalPosX = objPoint.x - devicePoint.x;
-
-    //var objFinalPosZ = 2;
-    //var objFinalPosX = 0;
-
+    let objFinalPosZ = objPoint.y - devicePoint.y;
+    let objFinalPosX = objPoint.x - devicePoint.x;
     let angle = this.state.heading * Math.PI/180;
     console.log("heading:" + this.state.heading);
     console.log("Cos: "+ Math.cos(angle));
@@ -186,26 +144,16 @@ export default class HelloWorldSceneAR extends Component {
                 Frente al H: 9.856204, -83.912598
                 Escuela Ciencias Sociales y Matemática: 9.856110, -83.913026
                 Pretil: 9.855782, -83.912626
+                Casa Verde: 9.937658, -84.074725
             */
-            let objetPositionAR_LabH = this._transformPointToAR(position.coords.latitude, position.coords.longitude,9.856204, -83.912598);
-            if(objetPositionAR_LabH.x < 5 || objetPositionAR_LabH.z < 5){
-              this.setState({
-                scaleObj1: [2,2,2]
-              })
-            }
-            let objetPositionAR_EscMate = this._transformPointToAR(position.coords.latitude, position.coords.longitude,9.856110, -83.913026);
-            let objetPositionAR_Pretil = this._transformPointToAR(position.coords.latitude, position.coords.longitude,9.855782, -83.912626);
+            let objetPositionAR = this._transformPointToAR(position.coords.latitude, position.coords.longitude,9.937658, -84.074725);
 
             this.setState({
               latitude: position.coords.latitude,
               longitude: position.coords.longitude,
-              objectXPosFrenteH: objetPositionAR_LabH.x,
-              objectZPosFrenteH: objetPositionAR_LabH.z,
-              objectXPosFrenteMate: objetPositionAR_EscMate.x,
-              objectZPosFrenteMate: objetPositionAR_EscMate.z,
-              objectXPosPretil: objetPositionAR_Pretil.x,
-              objectZPosPretil: objetPositionAR_Pretil.z,
-              coordinateXYZString: "[" + String(objetPositionAR_LabH.x) + ", 0, " + String(objetPositionAR_LabH.z) + "]",
+              objectXPos: objetPositionAR.x,
+              objectZPos: objetPositionAR.z,
+              coordinateXYZString: "[" + String(objetPositionAR.x) + ", 0, " + String(objetPositionAR.z) + "]",
               coordinateLatLongString: "Lat: " + String(position.coords.latitude) + " ** Lng: " + String(position.coords.longitude),
               error: null   
             });
