7286e2fbb4764ac8530ac2a1d14088f27c33c014
geraldma1998 <gerald.morales@pernixlabs.com>
2019-08-18 13:33:10 -0600

    Modified some style into email_register component

--- android/app/src/main/java/com/amonra/MainApplication.java
+++ android/app/src/main/java/com/amonra/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.agontuk.RNFusedLocation.RNFusedLocationPackage;
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.reactlibrary.RNSimpleCompassPackage;
@@ -44,6 +45,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new AsyncStoragePackage(),
             new RNGestureHandlerPackage(),
             new RNFirebasePackage(),
