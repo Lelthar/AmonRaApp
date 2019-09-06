cf828710584975b7759d9e4644b725d33dcf0d64
Jose <josevillalobos@ic-itcr.ac.cr>
2019-09-06 11:02:21 -0600

    Menu hamburguesa de map.js cambiado por componente importado

--- src/components/pages/map.js
+++ src/components/pages/map.js
@@ -105,7 +105,7 @@ const mapDispatchToProps = (dispatch) => {
   }
 };
 
-import hamburgerComponent from '../../../components/MenuSide/HamburguerMenu'
+import HamburgerComponent from '../../../components/MenuSide/HamburguerMenu'
 class MapComponent extends Component {
     constructor (props) {
         super(props);
@@ -557,38 +557,8 @@ class MapComponent extends Component {
             </View>
 
              {/* Start of hamburguer menu */}
-         {this.props.menuSideState &&
-        <View style={styles.hamburgerMenu}>
-          <SearchBar
-            round={true}
-            containerStyle={styles.containerInput}
-            inputContainerStyle={styles.searchBarInput}
-            inputStyle={styles.searchBarInput}
-            leftIconContainerStyle={styles.searchBarInput}
-            rightIconContainerStyle={styles.searchBarInput}
-            placeholder= "Busqueda"
-            placeholderTextColor='white'
-            lightTheme={true}
-            
-          />
-          <TouchableOpacity onPress={ onPressTextDiscover } >
-            <Text style={styles.textDiscover}>Descubri Barrio Amon</Text>
-          </TouchableOpacity>
-          
-            <View style={{backgroundColor:'#00A2B5'}}>
-              <TouchableOpacity onPress= {onPressTextDiscover}>
-                <Text style={styles.textList} >•Origen del Barrio</Text>
-              </TouchableOpacity>
-              <TouchableOpacity onPress= {onPressTextDiscover}>
-                <Text style={styles.textList} >•Vivencias</Text>
-              </TouchableOpacity>
-              <TouchableOpacity onPress= {onPressTextDiscover}>
-                <Text style={styles.textList} >•Arquitectura</Text>
-              </TouchableOpacity>
-            </View>
-          <Text style={styles.textMoreAmon}>Mas de Amon_RA</Text>
-        </View>
-        }
+              {this.props.menuSideState &&
+              <HamburgerComponent /> }
           {/*End of hamburguer menu*/}
           
                     
@@ -716,50 +686,7 @@ const styles = StyleSheet.create({
     marginTop: 92,
   },
 
-  // Styles for hamburguer menu:
-
-  searchBarInput:{
-    backgroundColor: '#10535D',
-
-  },
-  containerInput:{
-    backgroundColor: '#00A2B5'
-
-  },
-  textList:{
-    color: 'white',
-    textAlign: 'left',
-    fontSize: 14, 
-    backgroundColor: '#00A2B5',
-    padding:5,
-    marginLeft: 15
-  },
-  textMoreAmon:{
-    color: 'white',
-    textAlign: 'center',
-    fontSize:14,
-    backgroundColor: '#00A2B5',
-    padding:5
-
-  },
-
-  textDiscover:{
-    color: 'white',
-    textAlign: 'center',
-    fontSize:14,
-    backgroundColor: '#10535D',
-    padding:5
-  },
-
-  hamburgerMenu:{
-    backgroundColor:'#00A2B5',
-    height: 200,
-    width: 200,
-    position: 'absolute',
-    top: 0,
-    right: 0,
-
-  }
+ 
 
 });
 
