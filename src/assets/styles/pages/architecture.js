2fa2a9dec61149910d790bbd23c4eef34957b4d0
Jose <josevillalobos@ic-itcr.ac.cr>
2019-10-22 23:47:29 -0600

    Cambiadas dos columnas en arquitectura por una

--- src/assets/styles/pages/architecture.js
+++ src/assets/styles/pages/architecture.js
@@ -1,18 +1,34 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet,PixelRatio } from "react-native";
+
+const imageRatio = PixelRatio.getPixelSizeForLayoutSize(60);
+const fiveRatio = PixelRatio.getPixelSizeForLayoutSize(5);
+const sevenRatio = PixelRatio.getPixelSizeForLayoutSize(7);
 
 const styles = StyleSheet.create({
 
   container:{
     flex:1,
-    flexDirection: 'row',
+    flexDirection:'row',
+    alignItems: 'center',
   },
   image:{
-    height:150,
-    width:150,
+    flex: 1,
     backgroundColor: 'black',
-    margin:20
-
+    resizeMode: "stretch",
   },
+  list_item:{
+    height: imageRatio,
+    width: "50%",
+    padding: "2%",
+    backgroundColor:"#ffffff",
+  },
+  list_style: {
+    marginLeft: "4%",
+    marginRight: "4%",
+    paddingTop: "5%",
+    width:"93%",
+    justifyContent: "space-evenly",
+  }
 });
 
 export default styles;
