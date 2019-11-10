6bff389007908d31fca470a4865cc92ffac7d0ad
Jose <josevillalobos@ic-itcr.ac.cr>
2019-11-09 19:25:04 -0600

    Cambiada consulta de arquitectura y componentes para no usar valores alambrados

---
+++ src/assets/styles/partials/hamburgerMenu.js
@@ -0,0 +1,41 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  searchBarInput:{
+    backgroundColor: '#10535D',
+  },
+  containerInput:{
+    backgroundColor: '#00A2B5',
+  },
+  textList:{
+    color: 'white',
+    textAlign: 'left',
+    fontSize: 14, 
+    backgroundColor: '#00A2B5',
+    padding:5,
+    marginLeft: 15,
+  },
+  textMoreAmon:{
+    color: 'white',
+    textAlign: 'center',
+    fontSize:14,
+    backgroundColor: '#00A2B5',
+    padding:5,
+  },
+  textDiscover:{
+    color: 'white',
+    textAlign: 'center',
+    fontSize:14,
+    backgroundColor: '#10535D',
+    padding:5,
+  },
+  hamburgerMenu:{
+    backgroundColor:'#00A2B5',
+    width: 200,
+    position: 'absolute',
+    top: 0,
+    right: 0,
+  },
+});
+
+export default styles;
