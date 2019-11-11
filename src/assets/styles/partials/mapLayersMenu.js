a436b1944b93338440136aa086095cbc96dbe24f
lelthar <geraldma1998@gmail.com>
2019-11-10 18:09:33 -0600

    Change layers filter code

--- src/assets/styles/partials/mapLayersMenu.js
+++ src/assets/styles/partials/mapLayersMenu.js
@@ -2,26 +2,30 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   capasMenu: {
-    backgroundColor: 'white',
-    flex:20,
-    flexDirection:'row',
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
+    flex: 1,
+    flexDirection:"column",
+    backgroundColor:"white",
   },
-  imgContainer:{
-    flex:5,
+  imageStyles: {
+    height: "100%",
+    width: "100%",
+  },
+  imgContainerStyle:{
+    marginTop: 8,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imgContainerBarrioAmon:{
+    marginTop: 5,
+    marginBottom: 8,
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
   squareButton: {
     resizeMode:'contain',
-    flex:1,
+    flex: 1,
   },
 });
 
--- src/components/pages/Map.js
+++ src/components/pages/Map.js
@@ -94,6 +94,7 @@ const Map = (props) => {
   const [userData, setUserData] = useState(null);
   const [userLocation, setUserLocation] = useState(null);
   const [warningVisible, setWarningVisible] = useState(false);
+  const [featuresShowed, setFeaturesShowed] = useState([]);
 
   closeMenu = () => {
     props.resetAll();
@@ -118,6 +119,8 @@ const Map = (props) => {
 
   filterMarkersByActiveFilter = (selectedMarkers, filters) => {
     let activeMarkers = [];
+    let markersId = [];
+    //setFeaturesShowed([]);
 
     for (let i = 0; i < selectedMarkers.length; i++) {
       if (filters.includes(selectedMarkers[i].category)) {
@@ -135,10 +138,12 @@ const Map = (props) => {
           marker_id: 1,
           images_url: [selectedMarkers[i].image1_url, selectedMarkers[i].image2_url, selectedMarkers[i].image3_url],
         };
+        markersId.push(selectedMarkers[i].id);
         activeMarkers.push(newMarker);
       }
     }
 
+    //setFeaturesShowed()
     return activeMarkers;
   };
 
