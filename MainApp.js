dbbe76104c3b1a620bd217eb1fbfd13d06a38320
Casasola98 <jgomezcasasola@gmail.com>
2019-12-07 18:17:47 -0600

    F

--- MainApp.js
+++ MainApp.js
@@ -21,6 +21,7 @@ export default class MainApp extends Component {
         if (typeof this.props.navigation != "undefined"){
             firstTime = true;
         }
+        
         this.state = {
             current_screen: "None",
             navigation: {},
