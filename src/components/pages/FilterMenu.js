71e73da24b3df48c3db979a9d0e68771e36c88ba
Alexander Suarez <asuarez@ic-itcr.ac.cr>
2019-12-08 16:18:24 -0600

    conflictos

--- src/components/pages/FilterMenu.js
+++ src/components/pages/FilterMenu.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import {
     View,
     FlatList,
+    TouchableOpacity,
 } from 'react-native';
-import CheckBox from 'react-native-check-box'
-import Image from 'react-native-scalable-image'
-import { connect } from "react-redux";
+
 import {
   filterMenuAction,
   activeFiltersAction,
   menuResetAction,
 } from "../../redux/actions/menuDataActions";
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+import CheckBox from 'react-native-check-box'
+import Image from 'react-native-scalable-image'
+import { connect } from "react-redux";
 import styles from "../../assets/styles/pages/filterMenu";
 
 const mapStateToProps = state => {
@@ -51,110 +55,121 @@ const FEATURES_LIST = [
 
 class FilterMenu extends Component{
 
-    constructor(props){
-        super(props);
+  constructor(props){
+    super(props);
+    let categories = FEATURES_LIST;
+    let filter_categories = []
 
-        let categories = FEATURES_LIST;
-        let filter_categories = [];
-
-        //fetches each category from data.json and makes an array of objects with each category
-        for (category in categories){
-            filter_categories.push({key: categories[category], active: false});
-        };
-
-        this.state = {
-            filters: [],
-            filters: filter_categories,
-            allowDragging: true,
-        }
+    //fetches each category from data.json and makes an array of objects with each category
+    for (category in categories){
+      filter_categories.push({key: categories[category], active: false});
     }
 
-    componentDidMount(){
-        this.setFilters()
+    this.state = {
+      filters: [],
+      filters: filter_categories,
+      allowDragging: true,
+    };
+  }
+
+  componentDidMount(){
+    this.setFilters()
+  }
+
+  // Sets the available filters
+  setFilters(){
+    let activeFilters = this.props.activeFilters.slice()
+    let filters = this.state.filters.slice()
+
+    for (activeIndex in activeFilters){
+      if (activeFilters[activeIndex].active == true){
+        for (filterIndex in filters){
+          if (filters[filterIndex].key === activeFilters[activeIndex].key){
+            filters[filterIndex].active = true
+          }
+        }
+      }
     }
 
-    // Sets the available filters
-    setFilters(){
-        let activeFilters = this.props.activeFilters.slice()
-        let filters = this.state.filters.slice()
+    this.setState({
+      filters: filters
+    })
+  }
 
-        for (activeIndex in activeFilters){
-            if (activeFilters[activeIndex].active == true){
-                for (filterIndex in filters){
-                    if (filters[filterIndex].key === activeFilters[activeIndex].key){
-                        filters[filterIndex].active = true
-                    }
-                }
-            }
-        }
+  // Sets the state of the filter to active/innactive
+  toggleFilter(key){
+    let tmpFilters = this.state.filters.slice()
+    for(i in tmpFilters){
+      if (tmpFilters[i].key === key){
+        tmpFilters[i].active = !tmpFilters[i].active
 
         this.setState({
-            filters: filters
+          filters: tmpFilters
         })
+
+        this.sendActiveFilters();
+        return 1
+      }
     }
+    return -1
+  }
 
-    // Sets the state of the filter to active/innactive
-    toggleFilter(key){
-        let tmpFilters = this.state.filters.slice()
+  // Sends a list with the active filters to Navigator
+  sendActiveFilters(){
+      let tmpFilters = this.state.filters.slice()
+      let activeFilters = []
 
-        for(i in tmpFilters){
-            if (tmpFilters[i].key === key){
-                tmpFilters[i].active = !tmpFilters[i].active
+      for(i in tmpFilters){
+          if (tmpFilters[i].active == true){
+              activeFilters.push(tmpFilters[i])
+          }
+      }
 
-                this.setState({
-                    filters: tmpFilters
-                })
+      this.props.setActiveFilters(activeFilters);
+  }
 
-                this.sendActiveFilters();
-                return 1
-            }
-        }
-        return -1
-    }
 
-    // Sends a list with the active filters to Navigator
-    sendActiveFilters(){
-        let tmpFilters = this.state.filters.slice()
-        let activeFilters = []
+  getSelectionImage(source){
+      return (
+        <Image width={20} source={source} />
+      );
+  }
 
-        for(i in tmpFilters){
-            if (tmpFilters[i].active == true){
-                activeFilters.push(tmpFilters[i])
-            }
-        }
+  render() {
+      return (
+        
+        <View style={styles.container}>
+          <View style={styles.menu}>
 
-        this.props.setActiveFilters(activeFilters);
-    }
-
-    getSelectionImage(source){
-        return (
-            <Image width={20} source={source} />
-        );
-    }
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <View>                   
-                    <FlatList
-                        style={styles.menuBox}
-                        data={this.state.filters}
-                        renderItem = {({item}) => 
-                            <CheckBox
-                                style={ styles.filterStyle }
-                                rightText={item.key}
-                                rightTextStyle={styles.rightText}
-                                isChecked={item.active}
-                                onClick={() => this.toggleFilter(item.key)}
-                                checkedImage={this.getSelectionImage(CHECK_ICON)}
-                                unCheckedImage={this.getSelectionImage(UNCHECK_ICON)}
-                            />}
-                    />
-                    
-                </View>
+            <View style={styles.closeFilter}>
+              <TouchableOpacity onPress={() => this.props.resetAll()} >
+                <Icon
+                  name='times'
+                  size={20}
+                  color="#FFFFFF"
+                />
+              </TouchableOpacity>
             </View>
-        );
-    }
+
+            <FlatList
+              style={styles.menuBox}
+              data={this.state.filters}
+              renderItem={({item}) => 
+                <CheckBox
+                  style={ styles.filterStyle }
+                  rightText={item.key}
+                  rightTextStyle={styles.rightText}
+                  isChecked={item.active}
+                  onClick={() => this.toggleFilter(item.key)}
+                  checkedImage={this.getSelectionImage(CHECK_ICON)}
+                  unCheckedImage={this.getSelectionImage(UNCHECK_ICON)}
+                />}
+            />
+              
+          </View>
+        </View>
+      );
+  }
 }
 
 const filterMenuComponent = connect(mapStateToProps, mapDispatchToProps)(FilterMenu);
