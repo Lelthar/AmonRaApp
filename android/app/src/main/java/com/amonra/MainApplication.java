5dbad9eeaf84473235fef63562dfb2b6f5b7f5dd
Alexander Suarez <asuarez@ic-itcr.ac.cr>
2019-12-08 19:41:22 -0600

    deleted libs

--- android/app/src/main/java/com/amonra/MainApplication.java
+++ android/app/src/main/java/com/amonra/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.agontuk.RNFusedLocation.RNFusedLocationPackage;
 import com.facebook.react.ReactApplication;
-import com.horcrux.svg.SvgPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -12,23 +11,14 @@ import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.reactlibrary.RNSimpleCompassPackage;
 import com.airbnb.android.react.maps.MapsPackage;
-import co.apptailor.googlesignin.RNGoogleSigninPackage;
-import com.agontuk.RNFusedLocation.RNFusedLocationPackage;
-import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
-import com.airbnb.android.react.maps.MapsPackage;
 import android.content.Intent;
 import android.content.res.Configuration;
-
 import com.viromedia.bridge.ReactViroPackage;
-import com.reactlibrary.RNSimpleCompassPackage;
-
 import org.wonday.orientation.OrientationPackage;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -44,13 +34,11 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new SvgPackage(),
             new SplashScreenReactPackage(),
             new ReactVideoPackage(),
             new VectorIconsPackage(),
             new AsyncStoragePackage(),
             new RNGestureHandlerPackage(),
-            new RNGoogleSigninPackage(),
             new MapsPackage(),
             new OrientationPackage(),
             new RNFusedLocationPackage(),
