ff53159f69de788c87e2aee0090064c36873f6d7
Josue Suarez <josue.suarez@wundermanthompson.com>
2020-03-02 07:57:23 -0600

    no AR support screen

--- src/components/partials/ARNoSupport.js
+++ src/components/partials/ARNoSupport.js
@@ -2,29 +2,44 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
+  TouchableOpacity,
+  Linking,
+  Image,
 } from 'react-native';
 
-import localStyles from "../../assets/styles/partials/ARNoSupport";
+import styles from "../../assets/styles/partials/ARNoSupport";
 
-const txt_msg1 = "¡Lo sentimos!";
-const txt_msg2 = "Tu dispositivo no soporta la experiencia RA";
+const DEFAULT_MSG = "Necesitás el servicio ARCore de Google para activar la Realidad Aumentada";
+const DOWNLOAD_MSG = "Descargalo aquí:";
+const AR_CORE_LOGO = require('../../assets/images/augmentedReality/arcore-logo.png');
+const BRAND_IMAGE = require("../../assets/images/amonraBrand/marca-02-smaller.png");
 
 export default class VRSelectionMode extends Component {
     constructor(props) {
         super(props);
     }
 
+    goToPlayStoreARCoreLink = () => {
+      Linking.openURL("market://details?id=com.google.ar.core");
+    }
+
     render() { 
-        return( 
-            <View style={localStyles.outer} >
-            <View style={localStyles.inner} >
-
-            <Text style={localStyles.title}> {txt_msg1} {'\n'}</Text>
-            <Text style={localStyles.title}> {txt_msg2} {'\n'}</Text>
-
-            </View>
+      return( 
+        <View style={styles.mainContainer} >
+          <View style={styles.logoContainer}>
+            <Image style={styles.brandImage} source={BRAND_IMAGE} />
           </View>
-        );
+
+          <View style={styles.textContainer}>
+            <Text style={styles.message}> {DEFAULT_MSG} {'\n'}</Text>
+            <Text style={[styles.message, styles.bold]}> {DOWNLOAD_MSG} {'\n'}</Text>
+          </View>
+          
+          <TouchableOpacity onPress={this.goToPlayStoreARCoreLink} style={styles.button}>
+            <Image style={styles.arCoreImage} source={AR_CORE_LOGO}/> 
+          </TouchableOpacity>
+        </View>
+      );
     }
 
 }
